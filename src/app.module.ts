a46646aa90da3babc13d391265871fd11293a9f4
yzfgithub <1125864925@qq.com>
2020-01-11 21:43:33 +0800

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,21 +7,21 @@ import { AppService } from './app.service';
 // import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: 'root',
-    //   password: 'yanzf',
-    //   database:'yanzf',
-    //   entities: [__dirname + '/entities/*.entity{.ts,.js}'],
-    //   synchronize: true,
-    // }),
-    // RoleModule,
-    // UserModule,
-    // UploadModule
-  ],
+  // imports: [
+  //   TypeOrmModule.forRoot({
+  //     type: 'mysql',
+  //     host: 'localhost',
+  //     port: 3306,
+  //     username: 'root',
+  //     password: 'yanzf',
+  //     database:'yanzf',
+  //     entities: [__dirname + '/entities/*.entity{.ts,.js}'],
+  //     synchronize: true,
+  //   }),
+  //   RoleModule,
+  //   UserModule,
+  //   UploadModule
+  // ],
   controllers: [AppController],
   providers: [AppService],
 })
