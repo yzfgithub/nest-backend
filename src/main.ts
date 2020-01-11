397355752278c66d28f7c2e14aa0c88ef2ab33e9
yzfgithub <1125864925@qq.com>
2020-01-12 00:03:13 +0800

    save

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'
+import { NestExpressApplication } from '@nestjs/platform-express';
+import {join} from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname,'../public', 'upload'),{
+    prefix:'/static/'
+  })
   app.setGlobalPrefix('backend');
   app.useGlobalFilters(new HttpExceptionFilter());
 
--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -13,13 +13,13 @@ export class UploadService {
         const user = await this.userRepository.findOne({'name': body.username});
         for(const file of files) {
             // const url = '/data/image';
-            // const url = join(__dirname,'../../', 'public/upload'); // join(__dirname,'../../', 'public/image',${Date.now()}-${file.originalname});
-            const url2 = join(process.cwd(), '../../', '/image');
-            console.log(url2);
-            const writeImage = createWriteStream(url2)
+            const url = join(__dirname,'../../', 'public/upload'); // join(__dirname,'../../', 'public/image',${Date.now()}-${file.originalname});
+            // const url2 = join(process.cwd(), '../../', '/image');
+            console.log(url);
+            const writeImage = createWriteStream(url)
             writeImage.write(file.buffer)
             
-            this.imageRepository.save({user_id: user.user_id,url: url2})
+            this.imageRepository.save({user_id: user.user_id,url: url})
         }
         return '上传成功'
     }
