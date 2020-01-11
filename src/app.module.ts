469f11281aeb566205104ea9f8c15b86dc07704f
yzfgithub <1125864925@qq.com>
2020-01-11 21:41:59 +0800

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { RoleModule } from './role/role.module';
-import { UserModule } from './user/user.module';
-import { UploadModule } from './upload/upload.module';
+// import { TypeOrmModule } from '@nestjs/typeorm'
+// import { RoleModule } from './role/role.module';
+// import { UserModule } from './user/user.module';
+// import { UploadModule } from './upload/upload.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'yanzf',
-      database:'yanzf',
-      entities: [__dirname + '/entities/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-    RoleModule,
-    UserModule,
-    UploadModule
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'yanzf',
+    //   database:'yanzf',
+    //   entities: [__dirname + '/entities/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    // }),
+    // RoleModule,
+    // UserModule,
+    // UploadModule
   ],
   controllers: [AppController],
   providers: [AppService],
