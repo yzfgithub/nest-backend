acb8b04eae963eb3a7f704d12028603988d120ca
yzfgithub <1125864925@qq.com>
2020-01-12 00:13:35 +0800

    save

--- dist/main.js
+++ dist/main.js
@@ -4,12 +4,8 @@ const core_1 = require("@nestjs/core");
 const swagger_1 = require("@nestjs/swagger");
 const app_module_1 = require("./app.module");
 const http_exception_filter_1 = require("./common/filters/http-exception.filter");
-const path_1 = require("path");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.useStaticAssets(path_1.join(__dirname, '../public', 'upload'), {
-        prefix: '/static/'
-    });
     app.setGlobalPrefix('backend');
     app.useGlobalFilters(new http_exception_filter_1.HttpExceptionFilter());
     const options = new swagger_1.DocumentBuilder()
