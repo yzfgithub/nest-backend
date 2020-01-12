727d02835948853fe614769267003fd24aedc7f5
yzfgithub <1125864925@qq.com>
2020-01-12 14:32:42 +0800

    save

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -5,6 +5,7 @@ import { Image } from '../entities/image.entity'
 import { User } from '../entities/user.entity'
 import { Repository } from 'typeorm'
 import { InjectRepository } from '@nestjs/typeorm';
+var fs = require('fs');
 
 @Injectable()
 export class UploadService {
@@ -14,10 +15,15 @@ export class UploadService {
         for(const file of files) {
             // const url = '/data/image';
             // const url = join(__dirname,'../../../', 'public/upload'); // join(__dirname,'../../', 'public/image',${Date.now()}-${file.originalname});
-            const url2 = join(process.cwd(), '../../', '/image/');
-            const writeImage = createWriteStream(url2)
-            writeImage.write(file.buffer)
-            this.imageRepository.save({user_id: user.user_id,url: url2})
+            const url2 = join(process.cwd(), '../../', '/image/log.txt');
+            // const writeImage = createWriteStream(url2)
+            // writeImage.write(file.buffer)
+            // this.imageRepository.save({user_id: user.user_id,url: url2})
+            fs.writeFile(url2, {
+                a: '123'
+            }, (err) => {
+                console.log('写入成功');
+            })
         }
         return '上传成功'
     }
