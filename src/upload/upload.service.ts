fd1be003cc4db874ed1c396c6320779f0deae302
yzfgithub <1125864925@qq.com>
2020-01-12 14:51:21 +0800

    save

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -13,17 +13,11 @@ export class UploadService {
     async upload(files, body) {
         const user = await this.userRepository.findOne({'name': body.username});
         for(const file of files) {
-            // const url = '/data/image';
             // const url = join(__dirname,'../../../', 'public/upload'); // join(__dirname,'../../', 'public/image',${Date.now()}-${file.originalname});
-            const url2 = join(process.cwd(), '../../', '/image/log.txt');
+            const url2 = join(process.cwd(), '../../', '/image/'+file.originalname);
             const writeImage = createWriteStream(url2)
             writeImage.write(file.buffer)
             this.imageRepository.save({user_id: user.user_id,url: url2})
-            // fs.writeFile(url2, {
-            //     a: '123'
-            // }, (err) => {
-            //     console.log('写入成功');
-            // })
         }
         return '上传成功'
     }
