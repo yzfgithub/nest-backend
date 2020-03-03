3555aa5f77322dc5c47b8444ddb8109397456a79
yzfgithub <1125864925@qq.com>
2020-03-03 21:42:38 +0800

    save

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -5,11 +5,12 @@ import { News } from 'src/entities/news.entity';
 @Controller('news')
 export class NewsController {
     constructor(private readonly NewsService:NewsService){}
-    
+
     @Post()
     create(@Body() body:News): Promise<News> {
         return this.NewsService.create(body);
     }
+    
     @Delete(":news_id")
     remove(@Param() param){
         return this.NewsService.remove(param.news_id);
